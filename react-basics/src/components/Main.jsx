0bae2d53d2679c36c9141b91484819cc54bcfb38
taeyeonyoo <taeyeonyoo16@gmail.com>
2024-12-29 01:07:17 +0900

    완강

---
+++ react-basics/src/components/Main.jsx
@@ -0,0 +1,23 @@
+import './Main.css';
+
+const Main = () => {
+
+    const user = {
+        name: "유태연",
+        isLogin: true,
+    };
+
+    if(user.isLogin){
+        return <div className='logout'>로그아웃</div>;
+    } else {
+        return <div>로그인</div>
+    }
+
+//     return (
+//         <>
+//         {user.isLogin ? <div>로그아웃</div> : <div>로그인</div>}
+//         </>
+//     );
+  };
+
+  export default Main;
